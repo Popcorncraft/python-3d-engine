fdab162cea37b03f2caedada2ca43435c89ade9a
Popcorncraft <therealpopcorncraft@gmail.com>
2023-10-09 11:19:44 -0500

    fix way too large numbers

--- attempt_2.py
+++ attempt_2.py
@@ -3,16 +3,6 @@ import pygame
 import math
 import numpy as np
 
-#
-#
-#██████  ███████  █████  ██████       ██   ██ ███████ ██████  ███████ 
-#██   ██ ██      ██   ██ ██   ██      ██   ██ ██      ██   ██ ██      
-#██████  █████   ███████ ██   ██      ███████ █████   ██████  █████   
-#██   ██ ██      ██   ██ ██   ██      ██   ██ ██      ██   ██ ██      
-#██   ██ ███████ ██   ██ ██████       ██   ██ ███████ ██   ██ ███████ 
-#                                                                                
-# I belive that the problem is down near the bottom around line 174. More details down there
-
 #pygame setup
 pygame.init()
 pygame.font.init
@@ -72,6 +62,7 @@ matProj[3][3] = 0.0
 
 #multiply 3D vector by 4x4 matrix
 def MultiplyMatrixVector(input, matrix):
+    output = [0, 0, 0]
     output[0] = input[0] * matrix[0][0] + input[1] * matrix[1][0] + input[2] * matrix[2][0] + matrix[3][0]
     output[1] = input[0] * matrix[0][1] + input[1] * matrix[1][1] + input[2] * matrix[2][1] + matrix[3][1]
     output[2] = input[0] * matrix[0][2] + input[1] * matrix[1][2] + input[2] * matrix[2][2] + matrix[3][2]
@@ -82,12 +73,6 @@ def MultiplyMatrixVector(input, matrix):
         output[1] /= w
         output[2] /= w
     return(output)
-    
-#draw wire-frame triangle
-def DrawWireTriangle(p1x, p1y, p2x, p2y, p3x, p3y):
-    pygame.draw.line(screen, "white", (p1x, p1y), (p2x, p2y))
-    pygame.draw.line(screen, "white", (p2x, p2y), (p3x, p3y))
-    pygame.draw.line(screen, "white", (p3x, p3y), (p1x, p1y))
 
 #main loop
 while running == True:
@@ -99,6 +84,8 @@ while running == True:
     
     #clear screen
     screen.fill("black")
+
+    pygame.draw.circle(screen, "white", (0, 0), 100)
 
     #set up rotation matricies
     theta += 1 * pygame.time.get_ticks()
@@ -160,8 +147,6 @@ while running == True:
         triProjected[1][1] += 1
         triProjected[2][1] += 1
 
-        print(triProjected[0][0])
-
         triProjected[0][0] *= 0.5 * screen.get_width()
         triProjected[0][1] *= 0.5 * screen.get_height()
         triProjected[1][0] *= 0.5 * screen.get_width()
@@ -169,36 +154,10 @@ while running == True:
         triProjected[2][0] *= 0.5 * screen.get_width()
         triProjected[2][1] *= 0.5 * screen.get_height()
 
-        print(triProjected[0][0])
-
-        # ↑ These ↓ do the same thing. I have prints to test values. Before it gets fully scaled it is at ~3. After, it is ~80,000,000.
-        #         ↓                    It is semi-random due to some time based rotations I have going on but those shouldn't affect it. 
-        #         ↓                    But if it does the thing is on line 104. I have tried replacing screen.get_width() and screen.get_height()
-        #         ↓                    with the actual values but it didn't seem to change anything.
-        #         ↓
-        #         ↓                    The entire project is really just me trying to create a python version of Javidx9(One Lone Coder)'s cpp 3d engine.
-        #         ↓                    https://github.com/OneLoneCoder/Javidx9/blob/master/ConsoleGameEngine/BiggerProjects/Engine3D/OneLoneCoder_olcEngine3D_Part1.cpp
-        #         ↓                    https://youtu.be/ih20l3pJoeU?list=PLrOv9FMX8xJE8NgepZR1etrsU63fDDGxO&t=2283 Intended output
-        #                               
-        #                              
-
-
-        #triProjected[0][0] = triProjected[0][0] * (0.5 * screen.get_width())
-        #triProjected[0][1] = triProjected[0][1] * (0.5 * screen.get_height())
-        #triProjected[1][0] = triProjected[1][0] * (0.5 * screen.get_width())
-        #triProjected[1][1] = triProjected[1][1] * (0.5 * screen.get_height())
-        #triProjected[2][0] = triProjected[2][0] * (0.5 * screen.get_width())
-        #triProjected[2][1] = triProjected[2][1] * (0.5 * screen.get_height())
-
         #draw wire-frame
-        DrawWireTriangle(
-            triProjected[0][0],
-            triProjected[0][1],
-            triProjected[1][0],
-            triProjected[1][1],
-            triProjected[2][0],
-            triProjected[2][1],
-        )
+        #pygame.draw.line(screen, "white", (triProjected[0][0], triProjected[0][1]), (triProjected[1][0], triProjected[1][1]))
+        #pygame.draw.line(screen, "white", (triProjected[1][0], triProjected[1][1]), (triProjected[2][0], triProjected[2][1]))
+        #pygame.draw.line(screen, "white", (triProjected[2][0], triProjected[2][1]), (triProjected[0][0], triProjected[0][1]))
 
     #limit framerate
     clock.tick(60)
