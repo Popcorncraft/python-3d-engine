f41ca3a85581b5afcdb864a4c135b435fe1ac830
Popcorncraft <therealpopcorncraft@gmail.com>
2023-11-18 18:05:58 -0600

    fixed something i think

--- engine/main.py
+++ engine/main.py
@@ -21,11 +21,12 @@ theta = 0
 fov = 90
 triToRaster = []
 listTriangles = []
+tester = False
 
 #object init
-cube = meshObject(0, "engine/assets/cube.obj", [10, 0, 10, 1], [0, 0, 0, 1])
-axis = meshObject(1, "engine/assets/axis.obj", [-10, 0, 10, 1], [0, 0, 0, 1])
-objectList = [cube, axis]
+#axis = meshObject(0, "engine/assets/axis.obj", [0, 0, 5, 1], [0, 0, 0, 1])
+#objectList = [axis]
+mesh = createMeshFromOBJ("engine/assets/axis.obj")
 
 #pygame setup
 pygame.init()
@@ -63,7 +64,6 @@ while running == True:
     lookDir = vecMultMatrix(matCameraRot, target)
     target = vecAdd(camera, lookDir)
     matCamera = makePointAtMatrix(camera, target, upVec)
-    rightVec = vecCrossProduct(upVec, lookDir)
 
     #make view matrix from camera
     matView = invertMatrix(matCamera)
@@ -71,125 +71,126 @@ while running == True:
     #controls
     pressed = pygame.key.get_pressed()
     #movement
-    if pressed[pygame.K_w]:
-        camera = vecAdd(camera, vecMult(vecNormalize(lookDir), (cameraMoveVel * dt)))
-    if pressed[pygame.K_s]:
-        camera = vecAdd(camera, vecMult(vecNormalize(lookDir), -(cameraMoveVel * dt)))
-    if pressed[pygame.K_d]:
-        camera = vecAdd(camera, vecMult(vecNormalize(rightVec), (cameraMoveVel * dt)))
-    if pressed[pygame.K_a]:
-        camera = vecAdd(camera, vecMult(vecNormalize(rightVec), -(cameraMoveVel * dt)))
-    if pressed[pygame.K_SPACE]:
-        camera = vecAdd(camera, vecMult(vecNormalize(upVec), (cameraMoveVel * dt)))
-    if pressed[pygame.K_LSHIFT]:
-        camera = vecAdd(camera, vecMult(vecNormalize(upVec), -(cameraMoveVel * dt)))
+    #if pressed[pygame.K_w]:
+    #    camera = vecAdd(camera, vecMult(vecNormalize(lookDir), (cameraMoveVel * dt)))
+    #if pressed[pygame.K_s]:
+    #    camera = vecAdd(camera, vecMult(vecNormalize(lookDir), -(cameraMoveVel * dt)))
+    #if pressed[pygame.K_d]:
+    #    camera = vecAdd(camera, vecMult(vecNormalize(rightVec), (cameraMoveVel * dt)))
+    #if pressed[pygame.K_a]:
+    #    camera = vecAdd(camera, vecMult(vecNormalize(rightVec), -(cameraMoveVel * dt)))
+    #if pressed[pygame.K_SPACE]:
+    #    camera = vecAdd(camera, vecMult(vecNormalize(upVec), (cameraMoveVel * dt)))
+    #if pressed[pygame.K_LSHIFT]:
+    #    camera = vecAdd(camera, vecMult(vecNormalize(upVec), -(cameraMoveVel * dt)))
     #rotation
-    if pressed[pygame.K_LEFT]:
-        yaw -= cameraRotateVel * dt
-    if pressed[pygame.K_RIGHT]:
-        yaw += cameraRotateVel * dt
+    #if pressed[pygame.K_LEFT]:
+    #    yaw -= cameraRotateVel * dt
+    #if pressed[pygame.K_RIGHT]:
+    #    yaw += cameraRotateVel * dt
 
     triToRaster = []
     
-    for obj in objectList:
-        #make basic matricies
-        matRotX = makeXRotMatrix(obj.rotation[0])
-        matRotY = makeYRotMatrix(obj.rotation[1])
-        matRotZ = makeZRotMatrix(obj.rotation[2])
-        matTrans = makeTranslationMatrix(vecSub(obj.position, camera))
-        matProj = makeProjMatrix(fov, (screen.get_height() / screen.get_width()), 0.1, 1000)
+
+    #make basic matricies
+    matRotX = makeXRotMatrix(0)
+    matRotY = makeYRotMatrix(0)
+    matRotZ = makeZRotMatrix(0)
+    matTrans = makeTranslationMatrix([0, 0, 5])
+    matProj = makeProjMatrix(fov, (screen.get_height() / screen.get_width()), 0.1, 1000)
         
-        #create world matrix
-        matWorld = makeIdentityMatrix()
-        matWorld = matrixMultMatrix(matRotX, matRotZ)
-        matWorld = matrixMultMatrix(matWorld, matRotY)
-        matWorld = matrixMultMatrix(matWorld, matTrans)
+    #create world matrix
+    matWorld = makeIdentityMatrix()
+    matWorld = matrixMultMatrix(matRotZ, matRotX)
+    #matWorld = matrixMultMatrix(matWorld, matRotY)
+    matWorld = matrixMultMatrix(matWorld, matTrans)
 
-        for tri in obj.mesh:
-            #clear vectors
-            triProjected = [0, 0, 0, 1]
-            triTransformed = [0, 0, 0, 1]
-            triViewed = [0, 0, 0, 1]
+    for tri in mesh:
+        #clear vectors
+        triProjected = [0, 0, 0, 1]
+        triTransformed = [0, 0, 0, 1]
+        triViewed = [0, 0, 0, 1]
 
-            #world matrix transform
-            triTransformed[0] = vecMultMatrix(matWorld, tri[0])
-            triTransformed[1] = vecMultMatrix(matWorld, tri[1])
-            triTransformed[2] = vecMultMatrix(matWorld, tri[2])
+        #world matrix transform
+        triTransformed[0] = vecMultMatrix(matWorld, tri[0])
+        triTransformed[1] = vecMultMatrix(matWorld, tri[1])
+        triTransformed[2] = vecMultMatrix(matWorld, tri[2])
 
-            #calculate tri normal
-            normal = [0, 0, 0, 1]
-            line1 = [0, 0, 0, 1]
-            line2 = [0, 0, 0, 1]
+        #calculate tri normal
+        normal = [0, 0, 0, 1]
+        line1 = [0, 0, 0, 1]
+        line2 = [0, 0, 0, 1]
 
-            #calculate sides of triangle
-            line1 = vecSub(triTransformed[1], triTransformed[0])
-            line2 = vecSub(triTransformed[2], triTransformed[0])
+        #calculate sides of triangle
+        line1 = vecSub(triTransformed[1], triTransformed[0])
+        line2 = vecSub(triTransformed[2], triTransformed[0])
 
-            #cross product of lines to get normal of triangle
-            normal = vecCrossProduct(line1, line2)
+        #cross product of lines to get normal of triangle
+        normal = vecCrossProduct(line1, line2)
 
-            #normalize the normal
-            normal = vecNormalize(normal)
+        #normalize the normal
+        normal = vecNormalize(normal)
 
-            #get ray from triangle to camera
-            cameraRay = vecSub(triTransformed[0], camera)
+        #get ray from triangle to camera
+        cameraRay = vecSub(triTransformed[0], camera)
 
-            #if ray is aligned with normal the triangle is visible
-            if vecDotProduct(normal, cameraRay) < 0:
-                #illumination
-                lightDirection = [0, 1, -1]
-                lightDirection = vecNormalize(lightDirection)
+        #if ray is aligned with normal the triangle is visible
+        if vecDotProduct(normal, cameraRay) < 0:
+            #illumination
+            lightDirection = [0, 1, -1]
+            lightDirection = vecNormalize(lightDirection)
 
-                #how aligned the lightDirection and normal
-                dp = max(0.1, vecDotProduct(lightDirection, normal))
+            #how aligned the lightDirection and normal
+            dp = max(0.1, vecDotProduct(lightDirection, normal))
 
-                #set color
-                color = [dp, dp, dp]
+            #set color
+            color = [dp, dp, dp]
 
-                #convert world space to view space
-                triViewed[0] = vecMultMatrix(matView, triTransformed[0])
-                triViewed[1] = vecMultMatrix(matView, triTransformed[1])
-                triViewed[2] = vecMultMatrix(matView, triTransformed[2])
+            #convert world space to view space
+            triViewed[0] = vecMultMatrix(matView, triTransformed[0])
+            triViewed[1] = vecMultMatrix(matView, triTransformed[1])
+            triViewed[2] = vecMultMatrix(matView, triTransformed[2])
 
-                #clip viewed triangles angainst near plane
-                clippedTriangles = 0
-                clipResults = triClipAgainstPlane([0, 0, 0.1], [0, 0, 1], triViewed)
+            #clip viewed triangles angainst near plane
+            clippedTriangles = 0
+            clipResults = triClipAgainstPlane([0, 0, 0.1], [0, 0, 1], triViewed)
 
-                #loop over all triangles since some mmight have been created during clipping
-                for n in range(0, clipResults[0]):
+            #loop over all triangles since some might have been created during clipping
+            for n in range(0, clipResults[0]):
 
-                    #project tris
-                    triProjected[0] = vecMultMatrix(matProj, clipResults[n+1][0])
-                    triProjected[1] = vecMultMatrix(matProj, clipResults[n+1][1])
-                    triProjected[2] = vecMultMatrix(matProj, clipResults[n+1][2])
+                #project tris
+                triProjected[0] = vecMultMatrix(matProj, clipResults[n+1][0])
+                triProjected[1] = vecMultMatrix(matProj, clipResults[n+1][1])
+                triProjected[2] = vecMultMatrix(matProj, clipResults[n+1][2])
 
-                    #scale into view
-                    triProjected[0] = vecDiv(triProjected[0], triProjected[0][3])
-                    triProjected[1] = vecDiv(triProjected[1], triProjected[1][3])                
-                    triProjected[2] = vecDiv(triProjected[2], triProjected[2][3])
+                #scale into view
+                triProjected[0] = vecDiv(triProjected[0], triProjected[0][3])
+                triProjected[1] = vecDiv(triProjected[1], triProjected[1][3])                
+                triProjected[2] = vecDiv(triProjected[2], triProjected[2][3])
 
-                    #invert x and y
-                    triProjected[0][0] *= 1
-                    triProjected[1][0] *= 1
-                    triProjected[2][0] *= 1
-                    triProjected[0][1] *= 1
-                    triProjected[1][1] *= 1
-                    triProjected[2][1] *= 1
+                #invert x and y
+                triProjected[0][0] *= 1
+                triProjected[1][0] *= 1
+                triProjected[2][0] *= 1
+                triProjected[0][1] *= 1
+                triProjected[1][1] *= 1
+                triProjected[2][1] *= 1
 
-                    #offset view
-                    offsetView = [1, 1, 0]
-                    triProjected[0] = vecAdd(triProjected[0], offsetView)
-                    triProjected[1] = vecAdd(triProjected[1], offsetView)
-                    triProjected[2] = vecAdd(triProjected[2], offsetView)
-                    triProjected[0][0] *= 0.5 * screen.get_width()
-                    triProjected[0][1] *= 0.5 * screen.get_height()
-                    triProjected[1][0] *= 0.5 * screen.get_width()
-                    triProjected[1][1] *= 0.5 * screen.get_height()
-                    triProjected[2][0] *= 0.5 * screen.get_width()
-                    triProjected[2][1] *= 0.5 * screen.get_height()
+                #offset view
+                offsetView = [1, 1, 0]
+                triProjected[0] = vecAdd(triProjected[0], offsetView)
+                triProjected[1] = vecAdd(triProjected[1], offsetView)
+                triProjected[2] = vecAdd(triProjected[2], offsetView)
+                triProjected[0][0] *= 0.5 * screen.get_width()
+                triProjected[0][1] *= 0.5 * screen.get_height()
+                triProjected[1][0] *= 0.5 * screen.get_width()
+                triProjected[1][1] *= 0.5 * screen.get_height()
+                triProjected[2][0] *= 0.5 * screen.get_width()
+                triProjected[2][1] *= 0.5 * screen.get_height()
 
-                    #add tri to list for sorting
-                    triToRaster.append([triProjected, color])
+                #add tri to list for sorting
+                triToRaster.append([triProjected, color])
+
     
     triToRaster.sort(key=sortByAverageZ)
     
@@ -207,6 +208,7 @@ while running == True:
                 #grab triangle from front of queue
                 test = listTriangles.pop(0)
                 newTriangles -= 1
+                print(test)
 
                 #clip tri against a plane.
                 match p:
@@ -225,7 +227,9 @@ while running == True:
         
     
     for tri in listTriangles:
-        print(((tri[0][0], tri[0][1]), (tri[1][0], tri[1][1]), (tri[2][0], tri[2][1])))
+        #print(((tri[0][0], tri[0][1]), (tri[1][0], tri[1][1]), (tri[2][0], tri[2][1])))
         pygame.draw.polygon(screen, "white", ((tri[0][0], tri[0][1]), (tri[1][0], tri[1][1]), (tri[2][0], tri[2][1])))
+
+    pygame.display.flip()
 
 pygame.quit()
